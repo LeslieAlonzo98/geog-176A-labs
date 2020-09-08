42d118e5b3f74e83b354150ec588be43d6699569
Leslie <lesliealonzo31@yahoo.com>
2020-09-07 22:54:29 -0700

    safety commit

---
+++ docs/lab-05.R
@@ -0,0 +1,13 @@
+palobbwgs = palobb %>% 
+  st_transform(4326)
+
+palobb = st_bbox(palobbwgs)
+
+scenes = getlandsat::lsat_scenes()
+
+paloscene = scenes %>% 
+  filter(min_lat <= palobb$ymin, max_lat >= palobb$ymax,
+         min_lon <= palobb$xmin, max_lon >= palobb$xmax,
+         as.Date(acquisitionDate) == as.Date("2016-09-26"))
+
+write.csv(paloscene, file = "data/palo-flood.csv", row.names = FALSE)
